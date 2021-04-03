44fbbc1ae70f3c534f06f17e379e6f2207d729d5
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-04-03 19:11:46 +0200

    Add eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import Head from 'next/head'
+import { GlobalMeta } from '../components/GlobalMeta'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Natan Cieplinski</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css"></link>
-      </Head>
-    </div>
+    <>
+      <GlobalMeta /> 
+    </>
   )
 }
