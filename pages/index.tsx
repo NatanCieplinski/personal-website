fdee7dff688a2e7e8b8fd900e0ff893aef29a536
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-04-03 18:45:40 +0200

    Project config (#1)
    * Change favicon
    
    * Add TailwindCSS
    
    * Add css normalizer
    
    * Add typescript to project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Natan Cieplinski</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css"></link>
+      </Head>
+    </div>
+  )
+}
