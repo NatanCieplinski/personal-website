6c6f0b5f67b2541e92b9c13795899c307dd33f41
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-04-04 15:22:40 +0200

    Fix footer (#4)
    * Fix footer
    
    * Fix modern normalizer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import { GlobalMeta, Bio, Footer } from '../components/sections'
 
 const Home: React.FC = () => {
   return (
-    <div className="h-screen w-screen">
+    <>
       <GlobalMeta />
       <Bio /> 
       <Footer />
-    </div>
+    </>
   )
 }
 
