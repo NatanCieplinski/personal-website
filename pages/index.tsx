e8fcc7b07dddd31e14477c5bd5397fe7128ffc2b
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-04-04 13:08:27 +0200

    Add Footer (#3)
    * Add mobile responsiveness
    
    * Add social icons to footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import { GlobalMeta, Bio } from '../components/sections'
+import { GlobalMeta, Bio, Footer } from '../components/sections'
 
 const Home: React.FC = () => {
   return (
-    <>
+    <div className="h-screen w-screen">
       <GlobalMeta />
       <Bio /> 
-    </>
+      <Footer />
+    </div>
   )
 }
 
